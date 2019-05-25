05a45a7f2b012e7653edae0805a7b2a9201b8f71
Weiguang <chen.weiguang@gmail.com>
2019-05-26 02:19:35 +0800

    Initial HTTP req impl for provider (#1)
    * Use example code
    
    * Add working http req with test tf code
    
    * Run go mod tidy

---
+++ provider.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		DataSourcesMap: map[string]*schema.Resource{
+			"smallutil_http_req": dataHttpReq(),
+		},
+	}
+}
